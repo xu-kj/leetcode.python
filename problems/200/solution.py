b77e9a52f5257096b440cf987616792513b80da5
James Xu <james.xu.sh@live.com>
2021-03-16 23:46:23 -0700

    200 updated

--- problems/200/solution.py
+++ problems/200/solution.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(x: int, y: int) -> int:
@@ -9,7 +10,7 @@ class Solution:
                 return 0
             if grid[x][y] == '0':
                 return 0
-            
+
             grid[x][y] = '0'
             return 1 + dfs(x - 1, y) + dfs(x + 1, y) + dfs(x, y - 1) + dfs(x, y + 1)
 
